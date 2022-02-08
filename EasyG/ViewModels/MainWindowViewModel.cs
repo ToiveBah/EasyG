1ad6e40c684c9f1964359610f6ead1b5d1301d78
toivebah toivebah <toivebah@gmail.com>
2022-02-08 23:30:17 +0300

    initial commit

---
+++ EasyG/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using EasyG.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyG.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок окна
+        private string _Title = "EasyG";
+
+        /// <summary> Заголовок окна </summary>
+        public string Title
+        {
+           get => _Title;
+           // set 
+           // {
+              //  if(Equals(_Title, value)) return;
+              //  _Title = value;
+              //  OnPropertyChanged();
+            // }
+            set => Set(ref _Title, value);
+        }
+        #endregion
+    }
+}
---
+++ EasyG/Views/Windows/ProjectsNewWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EasyG.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ProjectsNewWindow.xaml
+    /// </summary>
+    public partial class ProjectsNewWindow : Window
+    {
+        public ProjectsNewWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
